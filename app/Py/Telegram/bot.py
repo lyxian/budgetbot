f9e259f033a2a188d890e3d5e2e78b9484b75cba
lyx <lyxlyxi@hotmail.com>
2021-12-23 17:12:38 +0800

    handle normal msg

--- app/Py/Telegram/bot.py
+++ app/Py/Telegram/bot.py
@@ -103,7 +103,7 @@ def createBot():
         bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
         bot.send_message(message.chat.id, "Choose Date", reply_markup=createMarkupCalendar())
 
-    @bot.message_handler(func=lambda message: 'reply_to_message' in vars(message).keys() and message.reply_to_message.json['from']['is_bot'] and "description" in message.reply_to_message.text)
+    @bot.message_handler(func=lambda message: message.reply_to_message and message.reply_to_message.json['from']['is_bot'] and "description" in message.reply_to_message.text)
     def message_handler(message):
         pattern = r'.*\$(\d+\.*\d*).* (.*) @ (.*)\n.*:(.*)'
         data = re.search(pattern, message.reply_to_message.text+message.text).groups()
